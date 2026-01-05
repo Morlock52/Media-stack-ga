f36c2027c623f3975017e9bec0d39d97613725ce
Morlock52 <morlok52@gmail.com>
2026-01-05 13:04:37 -0500

    auto-claude: 1.5 - Create docs-site/src/components/wizard/index.ts barrel file
    - Export WizardHeader, WizardProgressBar, WizardStepIndicator, WizardNavigation
    - Export all step components (QuickStartStep, StackSelectionStep, BasicConfigurationStep, AdvancedSettingsStep, ReviewGenerateStep)
    - Follows established pattern from docs/index.ts
    - Enables clean imports: import { WizardHeader } from '@/components/wizard'

---
+++ docs-site/src/components/wizard/index.ts
@@ -0,0 +1,12 @@
+// Wizard Shell Components
+export { WizardHeader } from './WizardHeader'
+export { WizardProgressBar } from './WizardProgressBar'
+export { WizardStepIndicator } from './WizardStepIndicator'
+export { WizardNavigation } from './WizardNavigation'
+
+// Wizard Step Components
+export { QuickStartStep } from './steps/QuickStartStep'
+export { StackSelectionStep } from './steps/StackSelectionStep'
+export { BasicConfigurationStep } from './steps/BasicConfigurationStep'
+export { AdvancedSettingsStep } from './steps/AdvancedSettingsStep'
+export { ReviewGenerateStep } from './steps/ReviewGenerateStep'
